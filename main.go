e612d522eceb0b8f1b5c7af794d3dfa768509bf3
sinmetal <metal.tie@gmail.com>
2019-05-09 19:19:40 +0900

    backendhellotimeにrequestを送るようにした

--- main.go
+++ main.go
@@ -3,23 +3,29 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
-	for {
-		res, err := http.Get("http://backendhellotime-service.default.svc.cluster.local:8080")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer res.Body.Close()
-		b, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(string(b))
+	http.HandleFunc("/", handler)
 
-		time.Sleep(5 * time.Second)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	res, err := http.Get("http://10.4.0.3:8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(b))
+
+	fmt.Fprintf(w, "Hello Backend %s", string(b))
 }
